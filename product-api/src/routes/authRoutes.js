f92bf329df241d29dc19ceff59f6695b62e886f6
Youness Manyani <164171383+YounessManyani@users.noreply.github.com>
2024-11-23 16:34:40 +0100

    Registration Enlever

--- product-api/src/routes/authRoutes.js
+++ product-api/src/routes/authRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { loginUser } = require('../controllers/authController');
 
 const router = express.Router(); // Création d'un routeur Express
 
-// Route pour l'inscription
-router.post('/register', registerUser);
 
 // Route pour la connexion
 router.post('/login', loginUser);
